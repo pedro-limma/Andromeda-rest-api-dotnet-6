1c266c05b2ee2e288081e36c3e902c2815df48f3
pedro-limma <pedro.lima.wk@gmail.com>
2021-11-11 09:54:29 -0300

    Initial commit

---
+++ src/Andromeda.Domain.Core/Models/Todo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andromeda.Domain.Core.Models
+{
+    public record Todo
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool Done { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+
+        public Todo(string Name, string description, )
+        {
+
+        }
+
+
+
+    }
+}
