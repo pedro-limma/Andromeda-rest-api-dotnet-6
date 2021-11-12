20be4f4e958b159f4cd63c175dc08c6579284ca4
pedro-limma <pedro.lima.wk@gmail.com>
2021-11-12 11:57:57 -0300

    Feature: Inicio da implementação do repository

---
+++ src/Adnromeda.Adapters.DAL/Repositories/Todo/TodoRepository.cs
@@ -0,0 +1,25 @@
+﻿using Andromeda.Adapters.DAL.Connection;
+using Andromeda.Adapters.DAL.UnitOfWork;
+using Andromeda.Domain.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andromeda.Adapters.DAL.Repositories.Todo
+{
+    public class TodoRepository : ITodoRepository
+    {
+        private DBSession _dBsession;
+        private IUnitOfWork _unit;
+
+        public TodoRepository(DBSession dBSession, IUnitOfWork unit)
+        {
+            _dBsession = dBSession;
+            _unit = unit;
+        }
+
+
+    }
+}
---
+++ src/Adromeda.Domain.Application/Interfaces/ITodoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Andromeda.Domain.Core.Models;
+
+namespace Andromeda.Domain.Application.Interfaces
+{
+    public interface ITodoRepository
+    {
+        Todo CreateTodo(Todo todo);
+        Todo UpdateTodo(Todo todo);
+        void DeleteById(int id);
+        List<Todo> GetAllTodos();
+        Todo GetById(int id);
+    }
+}
